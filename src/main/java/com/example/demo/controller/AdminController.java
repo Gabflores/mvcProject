49ab61170b35936a50bdf4ca06db7de5e4d47ef8
dev-renatto-pantoja <floresdelsante@gmail.com>
2022-08-06 19:59:34 -0500

    add cors on admin controller

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@CrossOrigin(origins="*",methods= {RequestMethod.GET,RequestMethod.POST})
 public class AdminController {
 
     private final AdminService adminService;
